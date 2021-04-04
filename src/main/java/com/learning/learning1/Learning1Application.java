03046b3b6ab25797531ae4b7d42d556b4c43207c
leonardo <leonardodimarchi@gmail.com>
2021-04-04 19:57:30 -0300

    first commit

---
+++ src/main/java/com/learning/learning1/Learning1Application.java
@@ -0,0 +1,13 @@
+package com.learning.learning1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/learning1/Learning1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.learning1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learning1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
